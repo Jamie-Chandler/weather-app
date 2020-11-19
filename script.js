f08306fd2118534e761d327916c013020a1db375
Jamie <jchandler678@gmail.com>
2020-11-19 13:32:46 +0000

    Added basic functionality for script.js file. Browser gets your lat and long info, and passes this into an API call.

---
+++ script.js
@@ -0,0 +1,60 @@
+window.addEventListener("load", () => {
+  let long;
+  let lat;
+  let temperatureDescription = document.querySelector('.temperature-description');
+  let temperatureDegree = document.querySelector('.temperature-degree');
+  let locationTimezone = document.querySelector('.location-timezone');
+  let imageIcon = document.querySelector('.fas');
+
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+
+      const apiStart = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=3794d71893a492265a7c7f132aadec6b&units=imperial
+      `;
+
+
+      fetch(apiStart)
+        .then((response) => {
+          return response.json();
+        })
+        .then(data => {
+          console.log(data);
+         const {temp}  = data.main;
+         const name  = data.name;
+         const weather = data.weather[0].description;
+         const icon = data.weather[0].main;
+
+         // Set DOM Elements from the API
+        temperatureDegree.textContent = temp;
+        temperatureDescription.textContent = weather;
+        locationTimezone.textContent = name;
+       
+        // Icon SVG image based on 'icon' variable.
+        if (icon === 'Thunderstorm'){
+          imageIcon.classList.add('fa-bolt');
+        } else if (icon === 'Drizzle'){
+          imageIcon.classList.add('fa-tint');
+        } else if (icon === 'Rain'){
+          imageIcon.classList.add('fa-cloud-showers-heavy');
+        } else if (icon === 'Snow'){
+          imageIcon.classList.add('fa-snowflake');
+        } else if (icon === 'Atmosphere'){
+          imageIcon.classList.add('fa-wind');
+        } else if (icon === 'Clear'){
+          imageIcon.classList.add('fa-sun');
+        } else if (icon === 'Clouds'){
+          imageIcon.classList.add('fa-cloud');
+         } else {
+          imageIcon.classList.add('fa-exclamation-triangle');
+         } 
+      });
+    }); 
+  }
+
+});
+
+
+// TO GET CELCIUS FROM FARENHEIT : (F − 32) × 5/9 = C
